2c72016acacd047eda6cc2982ae577faa82a3171
dinhphamcanh <dinhpc@solashi.com>
2023-12-24 00:05:55 +0700

    feat: detail product

--- src/components/screens/product/MainProduct.jsx
+++ src/components/screens/product/MainProduct.jsx
@@ -1,5 +1,6 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import React from "react";
+import ProductCard from "./ProductCard";
 
 function MainProduct() {
   return (
@@ -7,6 +8,28 @@ function MainProduct() {
       <Typography textAlign={"center"} fontWeight={"bold"} fontSize={24}>
         BEST SELLER
       </Typography>
+      <Box mt={4}>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4}>
+            <ProductCard />
+          </Grid>
+        </Grid>
+      </Box>
     </Box>
   );
 }
---
+++ src/components/screens/product/ProductCard.jsx
@@ -0,0 +1,118 @@
+import { Box, Rating, Stack, Typography, styled } from "@mui/material";
+import React from "react";
+import {
+  HiOutlineShoppingBag,
+  HiOutlineHeart,
+  HiOutlineEye,
+} from "react-icons/hi2";
+
+const Wrapper = styled(Box)({
+  position: "relative",
+  cursor: "pointer",
+
+  "&:hover": {
+    "& .overlay": {
+      opacity: 1,
+      background: "rgba(226, 226, 226,0.4)",
+      visibility: "visible",
+    },
+
+    "& .img": {
+      transform: "scale(1.25)",
+    },
+  },
+});
+
+const WrapImg = styled(Box)({
+  position: "relative",
+  cursor: "pointer",
+  maxWidth: 270,
+  maxHeight: 270,
+  overflow: "hidden",
+
+  "& .img": {
+    display: "block",
+    width: 270,
+    height: 270,
+    transition: "all 0.3s ease-in-out",
+    objectFit: "cover",
+  },
+
+  "& .overlay": {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    display: "flex",
+    justifyContent: "center",
+    gap: 16,
+    padding: "4px 0",
+    visibility: "hidden",
+    opacity: 0,
+    transition: "visibility 0s, opacity 0.3s linear",
+  },
+});
+
+const WrapIcon = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  background: "white",
+  width: 36,
+  height: 36,
+  borderRadius: "50%",
+  transition: "all 0.3s ease-in-out",
+  "& svg": {
+    color: "#000",
+    fontSize: 20,
+  },
+  "&:hover": {
+    background: "#000",
+    "& svg": {
+      color: "white",
+    },
+  },
+});
+
+function ProductCard() {
+  return (
+    <Wrapper>
+      <WrapImg>
+        <Box component={"img"} src={"/img/Product1.webp"} className="img" />
+        <Box className={"overlay"}>
+          <WrapIcon>
+            <HiOutlineEye />
+          </WrapIcon>
+          <WrapIcon>
+            <HiOutlineShoppingBag />
+          </WrapIcon>
+          <WrapIcon>
+            <HiOutlineHeart />
+          </WrapIcon>
+        </Box>
+      </WrapImg>
+
+      <Stack textAlign={"center"} gap={"2px"} mt={1}>
+        <Typography fontSize={14} fontWeight={"bold"}>
+          OXVA XLIM POD KIT
+        </Typography>
+        <Typography fontSize={10} color="red" fontWeight={"bold"}>
+          1,000,000 VNĐ
+        </Typography>
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          gap={1}
+        >
+          <Rating size="small" value={5} />
+          <Typography fontSize={12} fontWeight={"bold"}>
+            16 reviews
+          </Typography>
+        </Box>
+      </Stack>
+    </Wrapper>
+  );
+}
+
+export default ProductCard;
