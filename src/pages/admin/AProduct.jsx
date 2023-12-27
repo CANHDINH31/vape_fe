4226a48ec0652b200fac0053d1dc8b4125fd6cb4
dinhphamcanh <dinhpc@solashi.com>
2023-12-27 14:44:10 +0700

    feat:update ui

--- src/pages/admin/AProduct.jsx
+++ src/pages/admin/AProduct.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import AdminLayout from "../../components/layout/admin/AdminLayout";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, TextField, Grid } from "@mui/material";
+import ModalUpdate from "../../components/common/ModalUpdate";
 
 function AProduct() {
+  const [isOpenAdd, setIsOpenAdd] = useState(false);
+
   return (
     <AdminLayout>
       <Box
@@ -13,8 +16,30 @@ function AProduct() {
         <Typography fontWeight={"bold"} fontSize={20}>
           Quản lý sản phẩm
         </Typography>
-        <Button variant="contained">Thêm sản phẩm</Button>
+        <Button variant="contained" onClick={() => setIsOpenAdd(true)}>
+          Thêm sản phẩm
+        </Button>
       </Box>
+      <ModalUpdate open={isOpenAdd} title={"Thêm sản phẩm"} maxWidth={"lg"}>
+        <Grid container spacing={4}>
+          <Grid item xs={6}>
+            <Typography>Tên sản phẩm:</Typography>
+            <TextField fullWidth size="small" />
+          </Grid>
+          <Grid item xs={6}>
+            <Typography>Giá:</Typography>
+            <TextField fullWidth size="small" type="number" />
+          </Grid>
+          <Grid item xs={6}>
+            <Typography>Mô tả:</Typography>
+            <TextField fullWidth size="small" multiline rows={5} />
+          </Grid>
+          <Grid item xs={6}>
+            <Typography>Phân loại:</Typography>
+            <TextField fullWidth size="small" />
+          </Grid>
+        </Grid>
+      </ModalUpdate>
     </AdminLayout>
   );
 }
